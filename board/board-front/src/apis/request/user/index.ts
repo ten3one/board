3081b61dd99ee42d0cd2d3c92cfa1fd9869a6231
yjchu <yjchu@becomesolution.com>
2025-03-06 17:11:11 +0900

    유저 닉네임, 프로필 이미지 수정 API 연동

---
+++ board/board-front/src/apis/request/user/index.ts
@@ -0,0 +1,4 @@
+import UpdateNicknameRequestDto from "./update-nickname.request.dto";
+import UpdateProfileImageRequestDto from "./update-profileImage.request.dto";
+
+export type { UpdateNicknameRequestDto, UpdateProfileImageRequestDto };
--- board/board-front/src/apis/response/user/index.ts
+++ board/board-front/src/apis/response/user/index.ts
@@ -1,4 +1,11 @@
 import GetUserResponseDto from "./get-user.response.dto";
 import GetSignInUserResponseDto from "./get-sign-in-user.response.dto";
+import UpdateNicknameResponseDto from "./update-nickname.response.dto";
+import UpdateProfileImageResponseDto from "./update-profileImage.response.dto";
 
-export type { GetUserResponseDto, GetSignInUserResponseDto };
+export type {
+  GetUserResponseDto,
+  GetSignInUserResponseDto,
+  UpdateNicknameResponseDto,
+  UpdateProfileImageResponseDto,
+};
