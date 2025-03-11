f9a61396449ae73e27771335589106814ea5c52f
yjchu <yjchu@becomesolution.com>
2025-03-11 11:19:40 +0900

    개인정보 수정 api 구현

--- board/board-back/src/main/java/com/example/board_back/service/implement/UserServiceImplement.java
+++ board/board-back/src/main/java/com/example/board_back/service/implement/UserServiceImplement.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Service;
 
 import com.example.board_back.dto.request.user.UpdateNicknameRequestDto;
 import com.example.board_back.dto.request.user.UpdateProfileImageRequestDto;
+import com.example.board_back.dto.request.user.UpdateUserinfoRequestDto;
 import com.example.board_back.dto.response.ResponseDto;
 import com.example.board_back.dto.response.user.GetSignInUserResponseDto;
 import com.example.board_back.dto.response.user.GetUserResponseDto;
 import com.example.board_back.dto.response.user.UpdateNicknameResponseDto;
 import com.example.board_back.dto.response.user.UpdateProfileImageResponseDto;
+import com.example.board_back.dto.response.user.UpdateUserInfoResponseDto;
 import com.example.board_back.entity.UserEntity;
 import com.example.board_back.repository.UserRepository;
 import com.example.board_back.service.UserService;
@@ -115,5 +117,34 @@ public class UserServiceImplement implements UserService {
 
         return UpdateProfileImageResponseDto.success();
 
+    }
+
+    @Override
+    public ResponseEntity<? super UpdateUserInfoResponseDto> updateUserInfo(UpdateUserinfoRequestDto dto) {
+        try {
+
+            String email = dto.getEmail();
+            UserEntity userEntity = userRepository.findByEmail(email);
+            if (userEntity == null) {
+                return UpdateUserInfoResponseDto.notExistUser();
+            }
+
+            String nickname = dto.getNickname();
+            boolean existedNickname = userRepository.existsByNickname(nickname);
+            if (existedNickname) {
+                return UpdateUserInfoResponseDto.duplicateNickname();
+            }
+
+            String profileImage = dto.getProfileImage();
+
+            userEntity.setNickname(nickname);
+            userEntity.setProfileImage(profileImage);
+            userRepository.save(userEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+        return UpdateUserInfoResponseDto.success();
     }
 }
